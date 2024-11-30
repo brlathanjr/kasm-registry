7c8026d73f2fa1fe0d198d80838b43bdf6a1e9b1
brlathanjr <35091688+brlathanjr@users.noreply.github.com>
2024-11-30 02:48:11 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'BRLathanJr',
+    description: 'The store for BRLathanJr Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://brlathanjr.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/brlathanjr/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
